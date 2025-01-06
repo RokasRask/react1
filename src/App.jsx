9e40c8c66424b67f6ae4c4fd1d41120df238ef43
RokRask <rokas.raskevic@gmail.com>
2025-01-06 11:05:07 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,33 @@ import './app.css';
 import Bebras from './Bebras';
 
 function Zebras() {
-  return <h2>Zebras</h2>
+
+  const tagas = 'h2';
+
+  return (
+    <>
+      {
+        tagas === 'h2'
+          ?
+          <>
+            <h2>Zebras</h2>
+            <span>zebras</span>
+          </>
+          :
+          <h3>Zebras</h3>
+      }
+    </>
+  )
 }
 
 function App() {
   return (
     <div className="app">
       <header className="app-header">
+        <Zebras />
         <h1>React v18</h1>
-        <Bebras/>
-        <Zebras/>
+        <Bebras />
+        <Zebras />
       </header>
     </div>
   );
