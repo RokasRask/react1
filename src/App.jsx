16bb7634d6f8511a83fb907a8b434c610079de6b
RokRask <rokas.raskevic@gmail.com>
2025-01-08 13:40:09 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './app.css';
 import './buttons.scss';
 import Checkbox from './Components/044/Checkbox';
 import Mygtukai from './Components/044/Mygtukai';
+import Radio from './Components/044/Radio';
 import Select from './Components/044/Select';
 import Text from './Components/044/Text';
 
@@ -18,6 +19,8 @@ function App() {
                 <Select />
 
                 <Checkbox />
+
+                <Radio />
 
             </header>
         </div>
