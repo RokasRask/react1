4db88a7e0a9f7f471f71734efa750e24e5d0ec8a
RokRask <rokas.raskevic@gmail.com>
2025-01-09 11:35:31 +0200

    commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './app.css';
+import './buttons.scss';
+import List from './Components/045/List';
+
+function App() {
+
+
+    return (
+        <div className="app">
+            <header className="app-header">
+                
+                <List />
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
