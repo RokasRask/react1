8b2bd0c6726b4c0d3f4d2f70c5322582cd649db0
RokRask <rokas.raskevic@gmail.com>
2025-01-14 12:43:33 +0200

    commit

--- src/App05.jsx
+++ src/App05.jsx
@@ -1,6 +1,7 @@
 import './app.css';
 import './buttons.scss';
-import Bin from './Components/047/Bin';
+import List from './Components/045/List';
+import Sq from './Components/045/Sq';
 
 function App() {
 
@@ -9,7 +10,10 @@ function App() {
         <div className="app">
             <header className="app-header">
                 
-                <Bin />
+                {/* <List /> */}
+
+                <Sq />
+
 
             </header>
         </div>
