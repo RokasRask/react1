605c162121218077a487b969c5b26e7df2c1c3c0
RokRask <rokas.raskevic@gmail.com>
2025-01-15 12:28:50 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,56 @@
 import './app.css';
 import './buttons.scss';
-import Bin from './Components/047/Bin';
+import { useEffect, useState } from 'react';
+import rand from '.Functions/rand.js';
+import Square from './Components/048/Square';
 
 function App() {
 
-    console.log('App komponentas');
+    // console.log('App komponentas');
+
+    const [sq, setSq] = useState([]);
+    const [buttonDisabled, setButtonDisabled]
+
+    const disableEnableButton = d => {
+        setButtonDisabled(d);
+    }
+
+    useEffect (_ => {
+        if (sq.length >= 5) {
+            console.log('Per daug kvadratų');
+            disableEnableButton(true);
+        } else [
+            disableEnableButton(false);
+        ]
+    }, [sq])
+
+    const addSq = _ => {
+        setSq(s => [...s, {
+            id: Date.now(),
+            number: rand(1000, 9999)
+        }])
+    }
+
+    const deleteSq = id => {
+        setSq(s => s.filter(sq => sq.id !== id))
+    }
 
 
-  return (
-    <div className="app">
-      <header className="app-header">
+    return (
+        <div className="app">
+            <header className="app-header">
+                <div className='sq-bin'>
+                    {
+                        sq.map(s => <Square key={s.id}) />
+                    }
+                </div>
 
-        <Bin />
 
+                <button className='yellow' onClick={addSq} >ADD</button>
+            </header>
+        </div>
 
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/Components/048/Square.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+export default function Square({ sq }) {
+    useEffect(_ => {
+        console.log('NAUJAS Square komponentas', sq.number);
+
+        return _ => {
+            console.log('Square komponentas panaikintas', sq.number)
+        }
+
+    }, [sq.number]);
+
+    return (
+        <div className="sq" onClick={_ => deleteSq(sq.id)}>
+            {sq.number}
+        </div>
+    )
+}
