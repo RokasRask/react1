804e5399ba35d31e4a614da8d2b23dc4fe40170a
RokRask <rokas.raskevic@gmail.com>
2025-01-20 12:32:13 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import './crud.scss';
 import Create from './Components/crud/Create';
+import { useEffect, useState } from 'react';
 
 export default function App() {
 
     console.log('APP RENDER');
 
+    const [storeData, setStoreData] = useState(null);
 
+    useEffect(_ => {
+        if (null === storeData) {
+            return;
+        }
+
+        
+
+    }, [storeData])
     
     return (
         <div className="container">
             <div className="row">
                 <div className="col-4">
-                    <Create />
+                    <Create setStoreData={setStoreData}/>
                 </div>
                 <div className="col-8">
                     List
